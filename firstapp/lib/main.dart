25f7f8921011de14097d878a8facbb9c8f47d624
hamraskannur <kkhamras741@gmail.com>
2024-06-04 23:19:31 +0530

    40

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-      home: Scaffold(
-          body: Center(
-    child: Text('Hello world! '),
-  ))));
+  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Colors.deepPurple,
+        Color.fromARGB(246, 133, 130, 138),
+      ], begin: Alignment.bottomLeft, end: Alignment.bottomRight)),
+      child: const Center(
+          child: Text(
+        'Hello world!',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 28,
+        ),
+      )),
+    );
+  }
 }
