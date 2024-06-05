020c8d420b5317665590a139c095491b0c2cf084
hamraskannur <kkhamras741@gmail.com>
2024-06-05 18:35:35 +0530

    rollDice app completed

---
+++ firstapp/lib/gradient_container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:firstapp/dice_roll.dart';
+
+const startAlignment = Alignment.bottomLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.deepPurple;
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [color1, color2],
+              begin: startAlignment,
+              end: endAlignment)),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,27 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firstapp/gradient_container.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-        Colors.deepPurple,
-        Color.fromARGB(246, 133, 130, 138),
-      ], begin: Alignment.bottomLeft, end: Alignment.bottomRight)),
-      child: const Center(
-          child: Text(
-        'Hello world!',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 28,
-        ),
-      )),
-    );
-  }
+  runApp(const MaterialApp(
+      home: Scaffold(
+          body: GradientContainer(Color.fromARGB(255, 33, 5, 109),
+              Color.fromARGB(246, 68, 21, 149)))));
 }
