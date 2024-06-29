1c9b6c8e87a5dcbaa12e8e6b8d8d9ea705d0e281
hamraskannur <kkhamras741@gmail.com>
2024-06-29 21:51:56 +0530

    quiz app completed

---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/results_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  Widget? activeScreen;
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchSreen);
+    super.initState();
+  }
+
+  void onRestart() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = StartScreen(switchSreen);
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen =
+            ResultsScreen(chosenAnswers: selectedAnswers, onRestart: onRestart);
+      });
+    }
+  }
+
+  void switchSreen() {
+    setState(() {
+      activeScreen = QuestionsScreen(
+        onSelectAnsewr: chooseAnswer,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
---
+++ quiz_app/lib/results_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/question_summary/question_summary.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+    required this.onRestart,
+  });
+
+  final List<String> chosenAnswers;
+  final void Function() onRestart;
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i]
+      });
+    }
+    return summary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
+    return SizedBox(
+        width: double.infinity,
+        child: Container(
+          margin: const EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "You answered $numCorrectQuestions out $numTotalQuestions questions correctly",
+                style: GoogleFonts.lato(
+                  color: const Color.fromARGB(255, 230, 200, 253),
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Text("List of you answered questions...",
+                  style: GoogleFonts.lato(
+                    color: const Color.fromARGB(255, 230, 200, 253),
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  )),
+              const SizedBox(height: 30),
+              QuestionSummary(summaryData),
+              TextButton.icon(
+                  icon: const Icon(Icons.refresh),
+                  style: TextButton.styleFrom(foregroundColor: Colors.white),
+                  onPressed: onRestart,
+                  label: const Text("Restart quiz!"))
+            ],
+          ),
+        ));
+  }
+}
